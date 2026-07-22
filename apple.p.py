ab2ab501aee87befb31ff5499760ddea18369a08
Siddharthareddy5b1 <siddharthakoyagura68@gmail.com>
2026-07-22 14:25:14 +0530

    Add files via upload

---
+++ apple.p.py
@@ -0,0 +1,9 @@
+def add_commas(string):
+    string2=" "
+    for i in range(len(string)-1)
+    srting+=string[i]+","
+    srting2+=string[i+1]
+    return string2
+s=input("Enter a string:")
+print("Enter original string is: ",s)
+print("The string after adding commas:",add_commas(s))
